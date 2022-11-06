50750f25f34f936058f4123eaa4a46f3d6476a49
jobcmax <80607620+jobcmax@users.noreply.github.com>
2022-11-06 13:25:20 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-import { h, Component, render } from 'https://unpkg.com/preact?module';
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const d = new Date();
+let name = month[d.getMonth()];
+document.getElementById("app").innerHTML = "(Im doing a challenge called " + name + ". Its where i go through everyday of " + name + ".)";
